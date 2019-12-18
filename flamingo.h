1d633f1b2a2927e79961a4e051b8523348848fbe
Heiko Jehmlich <hje@jecons.de>
2019-12-18 15:21:06 +0100

    adjust timing

--- flamingo.h
+++ flamingo.h
@@ -1,7 +1,8 @@
 #define RX 					2
 #define TX  				0
 
-#define PULSE 				310
+// min 180 max 350
+#define PULSE 				250
 
 // we use 49 as the PRREMPT_RT use 50 as the priority of kernel tasklets and interrupt handler by default
 #define SCHED_PRIO 			49
