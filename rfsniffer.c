a031aefae28e5bde49f0b042848793e3bcb3bec1
Heiko Jehmlich <hje@jecons.de>
2021-04-23 15:40:37 +0200

    control via command line opts

--- rfsniffer.c
+++ rfsniffer.c
@@ -1,6 +1,6 @@
 /***
  *
- * Library for reading and decoding 433MHz messages
+ * sampling and decoding 433MHz messages
  *
  * Copyright (C) 04/2021 by teknoid
  *
@@ -27,10 +27,19 @@
 #include "flamingo.h"
 #include "utils.h"
 
-#define GPIO_PIN	27	//RPi2 Pin13 GPIO_GEN2
+#define GPIO_PIN			27	//RPi2 Pin13 GPIO_GEN2
 
-#define BUFFER		256
-#define MAX_EDGES	128 // max 64 bit code = 128 high/low edges
+#define BUFFER				256
+
+#define STATE_RESET			129 // max 64 bit code -> 128 high/low edges + 1
+
+#define PULSE_COUNTER_MAX	BUFFER * BUFFER
+
+static pthread_t thread_decoder;
+static void* decoder(void *arg);
+
+static pthread_t thread_sampler;
+static void* sampler(void *arg);
 
 // ring buffers
 static unsigned char sampler_index, decoder_index = 0;
@@ -39,17 +48,18 @@ static unsigned long sync_buffer[BUFFER];
 static unsigned long long code_buffer[BUFFER];
 
 // pulse counters: index equals pulse length, e.g. 100µs=1, 200µs=2, etc
-static unsigned long lpulse_counter[BUFFER * BUFFER];
-static unsigned long hpulse_counter[BUFFER * BUFFER];
+static unsigned long lpulse_counter[PULSE_COUNTER_MAX];
+static unsigned long hpulse_counter[PULSE_COUNTER_MAX];
+
+// default configuration
+static unsigned long sync_min = 1800;
+static unsigned long sync_max = 2000;
+static unsigned long bitdivider = 1500;
+static unsigned char decoder_delay = 3;
+static unsigned char sync_on_pulse = 0;
+static unsigned char bits_to_sample = 32;
 static unsigned char pulse_counter_active = 0;
-
-static pthread_t thread_decoder;
-static void* decoder(void *arg);
-
-static pthread_t thread_sampler;
-static void* sampler(void *arg);
-
-static unsigned char decoder_delay = 3; // standard: 3 seconds
+static unsigned char analyzer_mode = 0;
 
 // Ideen
 
@@ -70,9 +80,63 @@ static unsigned char decoder_delay = 3; // standard: 3 seconds
 // wenn erfolgreich beenden öffnen
 // wenn kill runter gezalt hat auch öffnen
 
+static void usage() {
+	printf("Usage: rfsniffer [-a] [-c] [-d X] [-l] [-h] [-x X] [-y X] [-z X]\n");
+	printf("  void  normal mode, sync on known pulses, sample and decode messages\n");
+	printf("  -a    analyzer mode\n");
+	printf("  -b X  bits to sample (0 - 64) default 32\n");
+	printf("  -c    activate pulse counter\n");
+	printf("  -d X  decoder delay in seconds, default 3\n");
+	printf("  -l    analyzer: sync on LOW pulses (default)\n");
+	printf("  -h    analyzer: sync on HIGH pulses\n");
+	printf("  -x X  analyzer: sync pulse min length in microseconds\n");
+	printf("  -y X  analyzer: sync pulse max length in microseconds\n");
+	printf("  -z X  analyzer: 0/1 bit divider pulse length in microseconds\n");
+}
+
 int main(int argc, char **argv) {
+
 	// test
 	printf("test 2x 0xdeadbeef = %s\n", printbits64(0xdeadbeefdeadbeef, 0x0101010101010101));
+
+	// parse command line arguments
+	if (argc > 0) {
+		int c;
+		while ((c = getopt(argc, argv, "ab:cd:lhx:y:z:")) != -1) {
+			switch (c) {
+			case 'a':
+				analyzer_mode = 1;
+				break;
+			case 'b':
+				bits_to_sample = atoi(optarg);
+				break;
+			case 'c':
+				pulse_counter_active = 1;
+				break;
+			case 'd':
+				decoder_delay = atoi(optarg);
+				break;
+			case 'l':
+				sync_on_pulse = 0;
+				break;
+			case 'h':
+				sync_on_pulse = 1;
+				break;
+			case 'x':
+				sync_min = strtoul(optarg, NULL, 0);
+				break;
+			case 'y':
+				sync_max = strtoul(optarg, NULL, 0);
+				break;
+			case 'z':
+				bitdivider = strtoul(optarg, NULL, 0);
+				break;
+			default:
+				usage();
+				return EXIT_FAILURE;
+			}
+		}
+	}
 
 	if (!bcm2835_init())
 		return -1;
@@ -212,10 +276,10 @@ static void decode_flamingo32(unsigned char i) {
 	printf(" :: Transmitter-Id = 0x%x, channel = %d, command = %d, payload = 0x%02x, \n", xmitter, channel, command, payload);
 }
 
-static void decode_test(unsigned char i) {
+static void decode_anaylzer(unsigned char i) {
 	unsigned long long code_raw = code_buffer[i];
 	unsigned long sync = sync_buffer[i];
-	printf("TEST sync %04lu 0x%08llx", sync, code_raw);
+	printf("ANALYZER sync %04lu 0x%08llx", sync, code_raw);
 	printf(" %s\n", printbits64(code_raw, 0x0101010101010101));
 }
 
@@ -234,8 +298,8 @@ static void decode(unsigned char index) {
 		return decode_flamingo24(index);
 	case 4:
 		return decode_flamingo32(index);
-	case 99:
-		return decode_test(index);
+	case 255:
+		return decode_anaylzer(index);
 	default:
 		printf("no decoder configured for pattern %d sync %lu 0x%0llx\n", pattern, sync_buffer[index], code_buffer[index]);
 	}
@@ -281,19 +345,18 @@ static void* sampler(void *arg) {
 	struct timeval tNow, tLast;
 	unsigned long long code;
 	unsigned long pulse, p1, p2;
-	unsigned int pulse_centi;
-	unsigned char mode, bits, state, kill, dummy;
-
-	mode = 0;							// normal mode: search SYN pulses
-//	mode = 255;							// counter mode: sample both HIGH/LOW pulses and count them
-
-	if (mode == 255) {
-		decoder_delay = 5; 				// to get more samples
-		pulse_counter_active = 1; 		// print counters
-	}
+	unsigned int pulse_counter;
+	unsigned char state, state_reset, bits, pin, dummy;
 
 	// initialize tLast for correct 1st pulse calculation
 	gettimeofday(&tLast, NULL);
+
+	state = 0;
+	if (analyzer_mode) {
+		state = 254;
+		printf("ANALYZER sync on %lu-%luµs %s pulses, sampling %d bits, 0/1 divider pulse length %luµs\n", sync_min, sync_max, sync_on_pulse ? "HIGH" : "LOW", bits_to_sample,
+				bitdivider);
+	}
 
 	while (1) {
 		// wait for interrupt
@@ -301,7 +364,7 @@ static void* sampler(void *arg) {
 
 		// sample time + pin state
 		gettimeofday(&tNow, NULL);
-		state = bcm2835_gpio_lev(GPIO_PIN);
+		pin = bcm2835_gpio_lev(GPIO_PIN);
 
 		// calculate length of last pulse, store timer value for next calculation
 		pulse = ((tNow.tv_sec * 1000000) + tNow.tv_usec) - ((tLast.tv_sec * 1000000) + tLast.tv_usec);
@@ -312,50 +375,67 @@ static void* sampler(void *arg) {
 		lseek(fdset[0].fd, 0, SEEK_SET);
 		read(fdset[0].fd, &dummy, 1);
 
-		// in counter mode we want all pulses
-		if (mode != 255) {
+		if (analyzer_mode || pulse_counter_active) {
+
+			// round pulse length to multiples of 100
+			p1 = pulse % 100;
+			p2 = p1 >= 50 ? (pulse - p1 + 100) : (pulse - p1);
+			pulse_counter = p2 / 100;
+
+			// update pulse counters: 100th pulse length is the array index
+			if (pulse_counter < PULSE_COUNTER_MAX) {
+				if (pin)
+					lpulse_counter[pulse_counter]++;
+				else
+					hpulse_counter[pulse_counter]++;
+			} else {
+				printf("warning: pulse_counter overflow %d\n", pulse_counter);
+			}
+
+		} else {
 
 			// ignore noise & crap
-			if (pulse < 222 || pulse > 22222)
+			if (pulse < 222)
+				continue;
+			if (pulse > 22222)
 				continue;
 
 			// error detection
-			if (--kill == 0) {
-				printf("sampling %d aborted\n", mode);
-				bits = 0;
-				mode = 0;
+			if (--state_reset == 0) {
+				printf("sampling %d aborted\n", state);
+				state = 0;
 				continue;
 			}
 		}
 
-		switch (mode) {
+		switch (state) {
 
-		case 0: // detect SYNC sequences and start sampling
+		case 0: // initial state: detect known SYNC pulses and define next sampling state
 			code = 0;
-			kill = MAX_EDGES + 1;
+			state_reset = STATE_RESET;
 
-			if (state) {
+			if (pin) {
 				// LOW sync pulses
-				if (3800 < pulse && pulse < 3999) {
+				if (3800 < pulse && pulse < 4000) {
 					// not a real sync - it's the pause between 1st and 2nd message
 					bits = 36;
-					mode = 1;
-					pattern_buffer[sampler_index] = mode;
+					state = 1;
+					pattern_buffer[sampler_index] = state;
 					sync_buffer[sampler_index] = pulse;
 				} else if (T1SMIN < pulse && pulse < T1SMAX) {
 					bits = 28;
-					mode = 2;
-					pattern_buffer[sampler_index] = mode;
+					state = 2;
+					pattern_buffer[sampler_index] = state;
 					sync_buffer[sampler_index] = pulse;
 				} else if (T4SMIN < pulse && pulse < T4SMAX) {
 					bits = 24;
-					mode = 3;
-					pattern_buffer[sampler_index] = mode;
+					state = 3;
+					pattern_buffer[sampler_index] = state;
 					sync_buffer[sampler_index] = pulse;
 				} else if (T2S1MIN < pulse && pulse < T2S1MAX) {
 					bits = 64;
-					mode = 4;
-					pattern_buffer[sampler_index] = mode;
+					state = 4;
+					pattern_buffer[sampler_index] = state;
 					sync_buffer[sampler_index] = pulse;
 				}
 			} else {
@@ -364,13 +444,13 @@ static void* sampler(void *arg) {
 			break;
 
 		case 1: // nexus - sample LOW pulses
-			if (bits && state) {
+			if (bits && pin) {
 				if (pulse > 1500)
 					code++;
 
 				if (--bits == 0) {
 					code_buffer[sampler_index++] = code;
-					mode = 0;
+					state = 0;
 				} else {
 					code <<= 1;
 				}
@@ -378,13 +458,13 @@ static void* sampler(void *arg) {
 			break;
 
 		case 2: // flamingo 28bit - sample HIGH pulses
-			if (bits && !state) {
+			if (bits && !pin) {
 				if (pulse > T1X2)
 					code++;
 
 				if (--bits == 0) {
 					code_buffer[sampler_index++] = code;
-					mode = 0;
+					state = 0;
 				} else {
 					code <<= 1;
 				}
@@ -392,13 +472,13 @@ static void* sampler(void *arg) {
 			break;
 
 		case 3: // flamingo 24bit - sample HIGH pulses
-			if (bits && !state) {
+			if (bits && !pin) {
 				if (pulse > T1X2)
 					code++;
 
 				if (--bits == 0) {
 					code_buffer[sampler_index++] = code;
-					mode = 0;
+					state = 0;
 				} else {
 					code <<= 1;
 				}
@@ -406,34 +486,60 @@ static void* sampler(void *arg) {
 			break;
 
 		case 4: // flamingo 32bit - sample LOW pulses
-			if (bits && state) {
+			if (bits && pin) {
 				code += pulse < T2Y ? 0 : 1;
 				if (--bits == 0) {
 					code_buffer[sampler_index++] = code;
-					mode = 0;
+					state = 0;
 				} else {
 					code <<= 1;
 				}
 			}
 			break;
 
-		case 254: // analyzer mode
-			// round pulse length to multiples of 100 and cut last 2 digits
-			p1 = pulse % 100;
-			p2 = p1 >= 50 ? (pulse - p1 + 100) : (pulse - p1);
-			pulse_centi = p2 / 100;
+		case 254: // analyzer sync mode: find the SYNC pulse
+			code = 0;
+			state_reset = STATE_RESET;
+
+			if (pin) {
+				// LOW sync pulses
+				if (sync_on_pulse == 0) {
+					if (sync_min < pulse && pulse < sync_max) {
+						bits = bits_to_sample;
+						state = 255; // next is analyzer sample mode
+						pattern_buffer[sampler_index] = state;
+						sync_buffer[sampler_index] = pulse;
+						printf("\nS ");
+					}
+				}
+			} else {
+				// HIGH sync pulses
+				if (sync_on_pulse == 1) {
+					if (sync_min < pulse && pulse < sync_max) {
+						bits = bits_to_sample;
+						state = 255; // next is analyzer sample mode
+						pattern_buffer[sampler_index] = state;
+						sync_buffer[sampler_index] = pulse;
+						printf("\nS ");
+					}
+				}
+			}
+			break;
+
+		case 255: // analyzer sample mode: sample bits and print
 			if (bits) {
-				code += pulse < 1500 ? 0 : 1;
+				if (pulse > bitdivider)
+					code++;
 
 				// print
-				if (state)
-					printf("L%02d ", pulse_centi);
+				if (pin)
+					printf("L%02d ", pulse_counter);
 				else
-					printf("H%02d ", pulse_centi);
+					printf("H%02d ", pulse_counter);
 
 				if (--bits == 0) {
 					code_buffer[sampler_index++] = code;
-					mode = 0;
+					state = 254; // back to analyzer sync mode
 					printf("\n");
 				} else {
 					code <<= 1;
@@ -441,24 +547,9 @@ static void* sampler(void *arg) {
 			}
 			break;
 
-		case 255: // counter mode
-			// round pulse length to multiples of 100 and used it as index for counters
-			p1 = pulse % 100;
-			p2 = p1 >= 50 ? (pulse - p1 + 100) : (pulse - p1);
-			pulse_centi = p2 / 100;
-			// update pulse counters
-			if (pulse_centi > (BUFFER * BUFFER)) {
-				printf("warning: pulse_centi overflow %d\n", pulse_centi);
-			} else {
-				if (state)
-					lpulse_counter[pulse_centi]++;
-				else
-					hpulse_counter[pulse_centi]++;
-			}
-			break;
-
 		default:
-			printf("no mode %d not defined\n", mode);
+			printf("illegal state %d\n", state);
+			state = 0;
 		}
 	}
 
@@ -471,50 +562,51 @@ static void* decoder(void *arg) {
 		return (void *) 0;
 	}
 
-	unsigned long maxh, maxl;
-	unsigned char maxhi, maxli;
+	unsigned long lmax, hmax;
+	unsigned char lmaxi, hmaxi;
+
+	printf("DECODER run every %d seconds\n", decoder_delay);
 
 	while (1) {
 		sleep(decoder_delay);
 
 		// print pulse counters and top ten pulse lengths, then clear counters
 		if (pulse_counter_active) {
-			printf("\nPulse counters of last %d seconds (H006 0327 means 327 HIGH pulses of 600µs, L019 0149 means 149 LOW pulses of 1900µs)\n", decoder_delay);
-			printf("\n");
+			printf("\nLCOUNTER up to 9900µs\n");
 			for (int i = 0; i < 100; i++) {
-				printf("L%02d  ", i);
+				printf("%02d   ", i);
 			}
 			printf("\n");
 			for (int i = 0; i < 100; i++) {
 				printf("%04lu ", lpulse_counter[i]);
 			}
-			printf("\n\n");
+			printf("\n\nHCOUNTER up to 9900µs\n");
 			for (int i = 0; i < 100; i++) {
-				printf("H%02d  ", i);
+				printf("%02d   ", i);
 			}
 			printf("\n");
 			for (int i = 0; i < 100; i++) {
 				printf("%04lu ", hpulse_counter[i]);
 			}
-			printf("\n\nTop Ten (without noise)\n");
+			printf("\n\nTOPTEN (without noise)\n");
 			lpulse_counter[0] = hpulse_counter[0] = 0;	// ignore pulses below 100µs
 			lpulse_counter[1] = hpulse_counter[1] = 0;	// ignore pulses below 200µs
 			hpulse_counter[2] = 0; 						// ignore HIGH pulses below 300µs
 			for (int m = 0; m < 10; m++) {
-				maxh = maxl = maxhi = maxli = 0;
+				hmax = lmax = hmaxi = lmaxi = 0;
 				for (int i = 0; i < BUFFER; i++) {
-					if (lpulse_counter[i] > maxl) {
-						maxl = lpulse_counter[i];
-						maxli = i;
+					if (lpulse_counter[i] > lmax) {
+						lmax = lpulse_counter[i];
+						lmaxi = i;
 					}
-					if (hpulse_counter[i] > maxh) {
-						maxh = hpulse_counter[i];
-						maxhi = i;
+					if (hpulse_counter[i] > hmax) {
+						hmax = hpulse_counter[i];
+						hmaxi = i;
 					}
 				}
-				printf("L%03d %04lu   H%03d %04lu\n", maxli, maxl, maxhi, maxh);
-				lpulse_counter[maxli] = 0;
-				hpulse_counter[maxhi] = 0;
+				printf("L%03d %04lu   H%03d %04lu\n", lmaxi, lmax, hmaxi, hmax);
+				lpulse_counter[lmaxi] = 0;
+				hpulse_counter[hmaxi] = 0;
 			}
 			for (int i = 0; i < 100; i++) {
 				lpulse_counter[i] = 0;
