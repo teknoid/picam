a3ec35cd8972b2bf79db7db00563a193d2e77252
Heiko Jehmlich <hje@jecons.de>
2021-04-30 00:08:27 +0200

    forgot

--- rfsniffer.h
+++ rfsniffer.h
@@ -1,10 +1,12 @@
+#include "rfcodec.h"
+
 #define GPIO_PIN			27	//RPi2 Pin13 GPIO_GEN2
 
-#define BUFFER				256
+#define BUFFER				0xff
 
 #define STATE_RESET			129 // max 64 bit code -> 128 high/low edges + 1
 
 #define PULSE_COUNTER_MAX	BUFFER * BUFFER
 
-int rfsniffer_init();
+int rfsniffer_init(rfsniffer_handler_t handler);
 int rfsniffer_close();
