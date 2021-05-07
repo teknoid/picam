845c784bd444d29bc1155245540ddbd6fb295160
Heiko Jehmlich <hje@jecons.de>
2021-05-07 11:53:20 +0200

    fix refresh mechanism

--- webcam/weather.js
+++ webcam/weather.js
@@ -1,6 +1,26 @@
-function updateData() {
+function updateMonitorix(when) {
 	var request = new XMLHttpRequest();
-	request.open("GET", "sensors.php");
+	if (typeof when == 'undefined')
+		request.open('GET', '/monitorix-cgi/monitorix.cgi?mode=localhost&graph=gensens');
+	else 
+		request.open('GET', '/monitorix-cgi/monitorix.cgi?mode=localhost&graph=gensens&when=' + when);
+	request.addEventListener('load', function(event) {
+		if (this.status == 200) {
+			var imgs = document.querySelectorAll('img');
+			Object.entries(imgs).forEach(([i, img]) => {
+				const token = img.src.replace(/^.*[\\\/]/, '').split('.');
+				const newwhen = typeof when != 'undefined' ?  when : token[1];
+				const newsrc = '/monitorix/imgs/' + token[0] + '.' + newwhen + '.png?ts=' + new Date().getTime();
+				img.src = newsrc;
+			});
+		}
+	});
+	request.send();
+}
+
+function updateCurrent() {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'sensors.php');
 	request.addEventListener('load', function(event) {
 		if (this.status == 200) {
 			var data = JSON.parse(this.responseText);
@@ -16,25 +36,22 @@ function updateData() {
 	request.send();
 }
 
-function show(e, interval) {
-	var intervals = document.querySelectorAll('#navigation-top > ul > li');
-	for (var i = 0; i < intervals.length; i++) {
-		intervals[i].style.background = '#fff';
+function show(e, when) {
+	var whens = document.querySelectorAll('#navigation-top > ul > li');
+	for (var i = 0; i < whens.length; i++) {
+		whens[i].style.background = '#fff';
 	}
 	e.style.background = '#efefef';
-	var imgs = document.querySelectorAll('img');
-	Object.entries(imgs).forEach(([i, img]) => {
-		const token = img.src.replace(/^.*[\\\/]/, '').split('.');
-		const newsrc = '/monitorix/imgs/' + token[0] + '.' + interval + '.png?ts=' + new Date().getTime();
-		img.src = newsrc;
-	});
+	updateMonitorix(when);
 }
 
 function update() {
-	updateData();
+	updateMonitorix();
+	updateCurrent();
 }
 
 window.onload = function() {
-	updateData();
+	updateMonitorix();
+	updateCurrent();
 	setInterval(update, 60000);
 }
