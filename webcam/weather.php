d7b5642706a10637541f0100742312ac5401ab99
Heiko Jehmlich <hje@jecons.de>
2021-12-27 12:08:02 +0100

    add more sensors

--- webcam/weather.php
+++ webcam/weather.php
@@ -1,18 +1,20 @@
-
 <?php
 header('Content-Type: application/json');
 
-$webcam = '/ram/webcam/';
-$nexus = '/ram/433/Nexus-TH/53/';
-$mcp3204 = '/ram/MCP3204/';
+$mtime = preg_replace("/\r|\n/", "", file_get_contents('/ram/webcam/.mtime'));
+$temp  = preg_replace("/\r|\n/", "", file_get_contents('/ram/433/Nexus-TH/53/temperature_C'));
+$humi  = preg_replace("/\r|\n/", "", file_get_contents('/ram/433/Nexus-TH/53/humidity'));
+$lumi  = preg_replace("/\r|\n/", "", file_get_contents('/ram/MCP3204/lum_percent'));
+$baro  = preg_replace("/\r|\n/", "", file_get_contents('/ram/433/RadioHead-ASK/0/payload/2'));
 
-if (file_exists($webcam) && file_exists($nexus)) {
-    $json = array(
-        'mtime' => str_replace(PHP_EOL, '', file_get_contents($webcam . '.mtime')),
-        'temp' => str_replace(PHP_EOL, '', file_get_contents($nexus . 'temperature_C')) . '°C',
-        'humi' => str_replace(PHP_EOL, '', file_get_contents($nexus . 'humidity')) . '%',
-        'lumi' => str_replace(PHP_EOL, '', file_get_contents($mcp3204 . 'lum_percent')) . '%'
-    );
-    echo json_encode($json);
-}
+$baro = ($baro + 101325) / 100;
+
+$json = array(
+	'mtime' => $mtime,
+	'temp'  => $temp . ' °C',
+	'humi'  => $humi . ' %',
+	'lumi'  => $lumi . ' %',
+	'baro'  => $baro . ' hPa'
+);
+echo json_encode($json);
 ?>
