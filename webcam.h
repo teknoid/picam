26c1cf75ec0f128882911e98eadad1b4da2efa8d
Heiko Jehmlich <hje@jecons.de>
2022-04-15 20:31:40 +0200

    webcam on/off to minimum

--- webcam.h
+++ webcam.h
@@ -4,10 +4,10 @@
 #define WEBCAM_STOP_TIMELAPSE	"su -c \"/home/www/webcam/webcam-stop.sh timelapse &\" hje"
 
 // webcam off: ↑earlier, ↓later
-#define WEBCAM_SUNDOWN			2
+#define WEBCAM_SUNDOWN			1
 
 // webcam on: ↑later ↓earlier
-#define WEBCAM_SUNRISE			2
+#define WEBCAM_SUNRISE			1
 
 int webcam_init(void);
 void webcam_close(void);
